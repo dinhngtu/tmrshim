1df06712e723db06901c0421f5c972e55cb10062
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2025-07-28 17:43:32 +0200

    minimize tmrdll
    Signed-off-by: Tu Dinh <1257909+dinhngtu@users.noreply.github.com>

--- tmrdll/dllmain.c
+++ tmrdll/dllmain.c
@@ -2,19 +2,10 @@
 #define NOMINMAX
 #include <windows.h>
 
-BOOL APIENTRY DllMain(
-    HMODULE hModule,
-    DWORD ul_reason_for_call,
-    LPVOID lpReserved) {
-    UNREFERENCED_PARAMETER(hModule);
-    UNREFERENCED_PARAMETER(lpReserved);
+BOOL WINAPI _DllMainCRTStartup(HINSTANCE const instance, DWORD const reason, LPVOID const reserved) {
+    UNREFERENCED_PARAMETER(instance);
+    UNREFERENCED_PARAMETER(reason);
+    UNREFERENCED_PARAMETER(reserved);
 
-    switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
     return TRUE;
 }
